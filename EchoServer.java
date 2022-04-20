16c95ecdb5e1e9dc4acad30122f6927ffe21e57a
Jajajajaz <104021481+Jajajajaz@users.noreply.github.com>
2022-04-21 00:23:33 +0800

    Add files via upload

---
+++ EchoServer.java
@@ -0,0 +1,29 @@
+package ServerEcho;
+
+import java.io.*;
+import java.net.*;
+
+
+public class EchoServer {
+	public static void main(String args[]){
+		System.out.println("Echo Server");
+		try(ServerSocket echoServer = new ServerSocket(8000)){	
+			Socket clientSocket = echoServer.accept();			
+			System.out.println("Connected to Client");	
+			BufferedReader bufferReader = new BufferedReader (new InputStreamReader(clientSocket.getInputStream()));	
+			PrintWriter outPut = new PrintWriter(clientSocket.getOutputStream(),true);	
+			String line;
+			while((line = bufferReader.readLine()) != null){	
+				System.out.println("\nMessage from Client: "+line);		
+				outPut.println(line);							
+				
+			}
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+
+}
